316c119931b3caf255b891fcd85ef9c0d17acde0
Edwin Somarribas Barahona <esb.somab@gmail.com>
2021-03-15 12:42:19 -0600

    cambios 1

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,6 @@ void casa() {
 Serial.println("Hola1");
 Serial.println("Hola2");
 Serial.println("Hola3");
-<<<<<<< HEAD
 Serial.println("Hola1");
 Serial.println("Hola2");
 Serial.println("Hola3");
@@ -30,9 +29,4 @@ Serial.println("Hola3");
 Serial.println("Hola1");
 Serial.println("Hola2");
 Serial.println("Hola3");
-
-=======
-Serial.println("Hola3");
-Serial.println("Hola4");
->>>>>>> 18a46b3ffe0e173c013b9d3e4cb76d85a9092a8c
 } 
